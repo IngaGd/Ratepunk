69dfb5760adc99923841751174ca035dc17569f6
Inga <inga.gudaite@gmail.com>
2023-06-14 09:55:52 +0300

    Header nav

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
-    return <div></div>;
+    return (
+        <>
+            <div>
+                <Link to="/ratepunk">Ratepunk</Link>
+                <Link to="/chrome-extension">Chrome Extension</Link>
+                <Link to="/price-comparison">Price Comparison</Link>
+                <Link to="/blog">Blog</Link>
+            </div>
+        </>
+    );
 }
