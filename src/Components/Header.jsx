97373a9ada3ee8226003fe1055f2b3156ddf24b9
Inga <inga.gudaite@gmail.com>
2023-06-18 13:43:28 +0300

    Dropdown menu

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,21 +3,18 @@ import { Link } from 'react-router-dom';
 import '../style/components/header.scss';
 import Logo from './Logo';
 
+import menuImg from '../assets/menu.svg';
+
 export default function Header() {
     return (
         <>
-            <div className="left">
-                <Link className="logo" to="/ratepunk">
-                    <Logo />
-                </Link>
-                {/* <div className="nav-mob">
-                    <button class="link">
-                        <i class="fa fa-bars" aria-hidden="true"></i>
-                    </button>
-                </div> */}
-            </div>
-            <div className="right">
-                <div className="nav-desktop">
+            <div className="nav-desktop">
+                <div className="left">
+                    <Link className="logo" to="/ratepunk">
+                        <Logo />
+                    </Link>
+                </div>
+                <div className="right">
                     <Link className="link" to="/chrome-extension">
                         <span>Chrome extension</span>
                     </Link>
@@ -27,6 +24,38 @@ export default function Header() {
                     <Link className="link" to="/blog">
                         Blog
                     </Link>
+                </div>
+            </div>
+            <div className="nav-mob">
+                <div className="nav-link">
+                    <Link className="logo" to="/ratepunk">
+                        <Logo />
+                    </Link>
+                </div>
+
+                <div className="nav-link">
+                    <button className="link">
+                        <img src={menuImg} alt="menu bar icon" />
+                    </button>
+                    <div className="dropdown">
+                        <div className="link-drop">
+                            <Link className="link" to="/chrome-extension">
+                                <span>Chrome extension</span>
+                            </Link>
+                        </div>
+
+                        <div className="link-drop">
+                            <Link className="link" to="/price-comparison">
+                                Price comparison
+                            </Link>
+                        </div>
+
+                        <div className="link-drop">
+                            <Link className="link" to="/blog">
+                                Blog
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
