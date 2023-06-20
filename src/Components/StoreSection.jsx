9909ffe5d5633075b4dbbdba35b966aeebe5fea5
Inga <inga.gudaite@gmail.com>
2023-06-20 17:32:16 +0300

    Style refactoring

--- src/Components/StoreSection.jsx
+++ src/Components/StoreSection.jsx
@@ -7,26 +7,30 @@ import appleStore from '../assets/apple.svg';
 export default function StoreSection() {
     return (
         <>
-            <a
-                className="chrome"
-                href="https://chrome.google.com/webstore/detail/ratepunk-same-hotel-way-c/gdaioanblcnghddimngklkhgcbomfdck?utm_source=ratepunk"
-            >
-                <img className="store" src={chromeStore} alt="" />
-                <div className="description">
-                    <p>available in the</p>
-                    <h5>chrome web store</h5>
-                </div>
-            </a>
-            <a
-                className="apple"
-                href="https://apps.apple.com/app/ratepunk/id1607823726"
-            >
-                <img className="store" src={appleStore} alt="" />
-                <div className="description">
-                    <p>available in the</p>
-                    <h5>chrome web store</h5>
-                </div>
-            </a>
+            <div>
+                <a
+                    className="chrome"
+                    href="https://chrome.google.com/webstore/detail/ratepunk-same-hotel-way-c/gdaioanblcnghddimngklkhgcbomfdck?utm_source=ratepunk"
+                >
+                    <img className="store" src={chromeStore} alt="" />
+                    <div className="description">
+                        <p>available in the</p>
+                        <h5>chrome web store</h5>
+                    </div>
+                </a>
+            </div>
+            <div>
+                <a
+                    className="apple"
+                    href="https://apps.apple.com/app/ratepunk/id1607823726"
+                >
+                    <img className="store" src={appleStore} alt="" />
+                    <div className="description">
+                        <p>available in the</p>
+                        <h5>chrome web store</h5>
+                    </div>
+                </a>
+            </div>
             <div className="apple-reviews">
                 {[...Array(5)].map((_, index) => (
                     <span className="star" key={index}>
